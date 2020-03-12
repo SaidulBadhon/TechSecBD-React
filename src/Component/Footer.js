4a7a7bccbbabe59287549dc36ede48ee10f8b3fe
Saidul Badhon <31236650+SaidulBadhon@users.noreply.github.com>
2020-03-13 04:47:10 +0600

    UI Edited.

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -1,9 +1,26 @@
 import React from 'react';
-
+import "./Footer.css";
+import { Typography } from '@material-ui/core';
 
 function Footer() {
     return (
-        <div>Footer</div>
+        <div>
+
+
+          <div className="FooterMain">
+          <Typography align="center" variant="h6" className="HaderTitle">
+            !!! IT CAN BE CHANGED TO ANYTHING !!!
+          </Typography>
+          </div>
+
+          <div className="BottomFooter">
+
+              <Typography align="center" variant="h6" className="HaderTitle">
+                © 2020 Copyright: TechSecBD.com
+              </Typography>
+          </div>
+        </div>
+
     )
 }
 
--- src/Pages/MainPages/HomePage/Content/ContentData.js
+++ src/Pages/MainPages/HomePage/Content/ContentData.js
@@ -12,36 +12,37 @@ const ContentData = [
         id: 1,
         img: Demo1,
         category: "DAILY NEWS",
-        heading: "Drinking coffee gives you",
-        seeMore: "Coffee is a hugely popular drink"
+        heading: "Drinking coffee gives you cancer?",
+        seeMore: "Coffee is a hugely popular drink, but many people " +
+         "     " + " are concerned that it may cause cancer. Others claim that drinking coffee offers health benefits and may even prevent cancer."
     },
     {
       id: 2,
-      img: Demo2,
-      category: "Weacklu NEWS",
-      heading: "Drinking coffee gives you",
-      seeMore: "Coffee is a hugely popular drink, many people are concerned that it may cause cancer."
+      img: Demo4,
+      category: "breaking news",
+      heading: "You can care about COVID-19 and also be sad ?",
+      seeMore: "When you live your life on the internet, it can feel like the novel coronavirus has brought the planet to a screeching halt. Coachella is canceled. E3 is canceled. South By Southwest is canceled."
     },
     {
       id: 3,
       img: Demo3,
       category: "Monthly NewS",
-      heading: "Drinking coffee gives you",
-      seeMore: "Coffee is a hugely popular drink, but many people are concerned that it may cause cancer. "
+      heading: "ACLU sues for records on facial recognition at the border !",
+      seeMore: "The American Civil Liberties Union has sued for information about the Trump administration’s controversial facial recognition programs."
     },
     {
       id: 4,
-      img: Demo4,
+      img: Demo2,
       category: "DAILY NEWS",
-      heading: "Drinking coffee gives you",
-      seeMore: "Coffee is a hugely popular drink, but many people are concerned that it may cause cancer."
+      heading: "A look at some of the hardest hit sectors in the S&P 500",
+      seeMore: "Wild swings in U.S. markets — mostly declines — are becoming the new normal during the coronavirus pandemic By — Seven weeks after"
     },
     {
       id: 5,
       img: Demo5,
       category: "DAILY NEWS",
-      heading: "Drinking coffee gives you",
-      seeMore: "Coffee is a hugely popular drink, but many people are concerned that it may cause cancer."
+      heading: "Coronavirus: How can AI help fight the pandemic?",
+      seeMore: "Image copyright Getty ImagesImage caption There is growing fear about the spread of the virus Executives from Amazon, Google, Microsoft,"
     },
     {
       id: 6,
